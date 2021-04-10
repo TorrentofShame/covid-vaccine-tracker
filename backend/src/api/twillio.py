5a9cbac1f255db9566eea2a74147535a567aa695
zenith110 <thequake@live.com>
2021-04-10 18:57:18 -0400

    Added twillio data

--- backend/src/api/twillio.py
+++ backend/src/api/twillio.py
@@ -7,9 +7,9 @@ subscriber_blueprint = Blueprint("twillio", __name__)
 @subscriber_blueprint.route("/twillio/send_message/", methods=["GET"])
 def get_subscriber():
     # Your Account SID from twilio.com/console
-    account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    account_sid = "ACd6b7da737e34c93de6ce6d1347425e52"
     # Your Auth Token from twilio.com/console
-    auth_token = "your_auth_token"
+    auth_token = "02948ba94bb4030e6e855465345d19c5"
     client = Client(account_sid, auth_token)
     # Gets the cell phone from getting posted
     cell_number = request.data.get("phone_number")
@@ -21,6 +21,6 @@ def get_subscriber():
     if(subscriber.vaccine_site[-1].available is True):
         message = client.messages.create(
             to="+1" + cell_number, 
-            from_="+15017250604",
+            from_="+13174015072",
             body="Vaccine appointments are available at " + subscriber.vaccine_site[-1].name + " on " + 
             subscriber.vaccine_site[-1].address)
