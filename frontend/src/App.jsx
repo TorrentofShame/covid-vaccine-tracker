845083acbb1ca87e71fc208f1906514534f3b64a
suneettipirneni <suneettipirneni@icloud.com>
2021-04-10 14:32:32 -0400

    Add more map stuffs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 import VaccineMap from './components/VaccineMap';
 import './scss/App.scss';
 
+const staticData = [
+  {
+      name: "Publix",
+      position: { lat: 24, lng: 32},
+      available: true,
+  },
+  {
+      name: "Publix",
+      position: { lat: 24, lng: 1},
+      available: false,
+  },
+  {
+      name: "Publix",
+      position: { lat: 24, lng: 5},
+      available: true,
+  }
+]
+
 function App() {
   return (
     <div className="App">
-      <VaccineMap />
+      <VaccineMap data={staticData} />
     </div>
   );
 }
